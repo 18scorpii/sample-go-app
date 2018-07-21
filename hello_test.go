d932ce05cb56586f33684caca3af73c22145ee53
kamal <kamal.ny@gmail.com>
2018-07-21 19:09:33 -0400

    added testcase

---
+++ hello_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestPrintMessage(t *testing.T)  {
+	expected := "Hello World !!"
+	got := PrintMessage()
+	if got != expected{
+		t.Errorf("Message received [%s] not same as expected [%s]", got, expected)
+	}
+	}
